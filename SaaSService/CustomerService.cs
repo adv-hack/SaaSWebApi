a29b84a83f34fdd96fe01c3d62f69ba659ebffbe
Krutika Khatavkar <krutika.khatavkar@onedavanced.com>
2018-08-03 15:31:51 +0530

    changes

---
+++ SaaSService/CustomerService.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SaaSDTO;
+using SaaSDAL;
+
+namespace SaaSService
+{
+    public class CustomerService
+    {
+        public List<Customers> GetCustomers()
+        {
+            List<Customers> customers = null;
+            using (HackSaaSEntities context = new HackSaaSEntities())
+            {
+                try
+                {
+                    customers = context.Customers.ToList();
+                }
+                catch (System.Exception)
+                { }
+            }
+
+            return customers;
+        }
+
+
+    }
+}
---
+++ SaaSWebApi/Controllers/CustomerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Web.Http;
+using SaaSDTO;
+using SaaSDAL;
+using SaaSService;
+using System.Web;
+using System.Text;
+using SaaSWebAPI;
+using System.Collections.Generic;
+
+namespace SaaSWebApi.Controllers
+{
+    [Authenticate]
+    public class CustomerController : ApiController
+    {
+        // GET: Customer
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        [HttpPost]       
+        public SaaSResponse Customer(string uname, string pin)
+        {
+            SaaSResponse objResponseXML = new SaaSResponse();
+            List<SaaSCustomer> oCustomers = new List<SaaSCustomer>();
+            //oCustomers.GetCustomers();
+            //oCustomers.Name = "Krutika";
+            //oCustomers.Email = "krutika@gmail.com";
+            //oCustomers.Mobile = "674983";
+            //oCustomers.Address = "hskdjfhkj";
+            //oCustomers.Notes = "adgfjdagfj";
+            CustomerService customer = new CustomerService();
+            var customerContext = customer.GetCustomers();
+            foreach(var item in customerContext )
+            {
+                oCustomers.Add(new SaaSCustomer() { Name = item.Name, Email = item.Email, Mobile = item.Mobile, Address = item.Address, Notes = item.Notes });
+            }
+            objResponseXML._saasData = Common.GetXMLFromObject<List<SaaSCustomer>>(oCustomers);
+            return objResponseXML;
+        }
+
+    }
+}
